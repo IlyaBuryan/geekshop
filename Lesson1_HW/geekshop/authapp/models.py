1cc2636fd539fac8933f4bda2ea52a685c0f907d
ilya <burian-iliyar@yandex.ru>
2021-06-16 14:46:17 +0300

    authentication + confirmation

--- Lesson1_HW/geekshop/authapp/models.py
+++ Lesson1_HW/geekshop/authapp/models.py
@@ -8,11 +8,10 @@ class ShopUser(AbstractUser):
     avatar = models.ImageField(upload_to='users_avatars', blank=True)
     age = models.PositiveIntegerField(verbose_name='возраст')
 
-    # activation_key = models.CharField(max_length=128, blank=True)
-    # activation_key_expires = models.DateTimeField(default=(now() + timedelta(hours=48)))
-    #
-    # def is_activation_key_expired(self):
-    #     if now() <= self.activation_key_expires:
-    #         return False
-    #     return True
-    #
+    activation_key = models.CharField(max_length=128, blank=True)
+    activation_key_expires = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+
+    def is_activation_key_expired(self):
+        if now() < self.activation_key_expires + timedelta(hours=48):
+            return False
+        return True
