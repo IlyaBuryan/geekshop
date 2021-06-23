60b6228733120f646d60eb871450620a0fc2a567
Ilya Buryan <burian-iliyar@yandex.ru>
2021-06-23 15:20:52 +0300

    Lesson3 ex 1-5

--- Lesson3_HW/geekshop/geekshop/urls.py
+++ Lesson3_HW/geekshop/geekshop/urls.py
@@ -15,6 +15,8 @@ urlpatterns = [
     re_path(r'^admin/', include('adminapp.urls', namespace='admin')),
 
     path('', include('social_django.urls', namespace='social')),
+
+    path('order/', include('ordersapp.urls', namespace='order'))
 ]
 
 if settings.DEBUG:
